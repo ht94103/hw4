8a0fc8d402a113b9fb60a894166ec7ebcc7a4e98
ht94103 <61787600+ht94103@users.noreply.github.com>
2020-06-06 23:55:17 +0800

    3rd update, under wifi_mqtt folder

--- mqtt_client.py
+++ mqtt_client.py
@@ -29,24 +29,27 @@ topic= "Mbed"
 # Callbacks
 def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
-t0 = time.time()
+
 
 def on_message(mosq, obj, msg):
 	mess=str(msg.payload)
+# save data after reading the first indicator
 while (1):
 	line = s.readline()
-	if float(line) == 999.9:
+	if float(line) == 999.5:
 		line = s.readline()
 		ind.append(float(line))
 		break;
-	if float(line) == 111.1:
+	if float(line) == 111.5:
 		line = s.readline()
 		ind.append(float(line))
 		break;
 
+t0 = time.time()
 while (1):
 	sample_time = time.time()
-	if sample_time-t0 < 20.1:
+	if sample_time-t0 < 20:
+		t.append(sample_time-t0)
 		line = s.readline()
 		data.append(float(line))
 		line = s.readline()
@@ -55,36 +58,20 @@ while (1):
 		data.append(float(line))
 		line = s.readline()
 		ind.append(float(line))
-		t.append(sample_time-t0)
+
 	else :
 		break
 
 ind.append(ind[len(ind) - 1])
-x.append(data[0])
-y.append(data[1])
-z.append(data[2])
-timestamp.append(t[0])
 
 for i in range (int(len(ind) - 1)):
 	p = ind[i]
 	k = ind[i + 1]
-	if k != p:
+	if k != p:   #save data to x, y, z lists when the indicator changes
 		x.append(data[3*i])
 		y.append(data[3*i + 1])
 		z.append(data[3*i + 2])
-		timestamp.append(i)
-
-//print(len(x), len(y), len(z), len(timestamp), len(ind))
-#print(timestamp)
-#print('\n')
-#print(data)
-    #print(" Message: " + str(msg.payload) + "\n");
-	#for i in range (2):
-		#s =   str(msg.payload)
-		#s1=s[2:-5]
-		#s2=s1.split(',')
-		#x.append(eval(s2[2]))
-	#print(x)
+		timestamp.append(t[i])
 
 def on_subscribe(mosq, obj, mid, granted_qos):
 
@@ -104,16 +91,8 @@ print("Connecting to " + host + "/" + topic)
 mqttc.connect(host, port=1883, keepalive=60)
 mqttc.subscribe(topic, 0)
 
-# Loop forever, receiving messages
-#mqttc.loop_forever()
-
-#print("rc: " + str(rc))
-
-
-
 plt.plot(timestamp, x, color = "blue", label = "x-acc")
 plt.ylim(-1.5, 1.5)
-#plt.xlim(0, 20)
 plt.plot(timestamp, y, color = "red", label = "y-acc")
 plt.plot(timestamp, z, color = "green", label = "z-acc")
 plt.xlabel("timestamp")
